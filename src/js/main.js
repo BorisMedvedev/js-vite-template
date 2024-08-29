314442ecb966937dc7e3d7be647ca02c74173518
John Doe <johndoe@example.com>
2024-08-29 13:57:46 +0300

    fix

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+import '../scss/style.scss';
+
+const loadJsonData = async () => {
+  try {
+    const response = await fetch('data.json');
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Ошибка при загрузке данных:', error);
+  }
+}
+
+loadJsonData().then(data => {
+  console.log(data);
+});
+
+
